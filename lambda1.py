e88a95885012ab7ede701fe582ef8e74c974dee0
tikejhya <aashisn@hotmail.com>
2017-11-28 22:04:52 +0000

    hmmm
    hmmm

--- lambda1.py
+++ lambda1.py
@@ -14,6 +14,7 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     detail_type =  str(event['detail-type'])
+    event_source = str(event['Records'][0]['EventSource'])
     
     if detail_type == "AWS API Call via CloudTrail":
         detail_type =  str(event['detail-type'])
@@ -23,6 +24,19 @@ def lambda_handler(event, context):
             'channel': SLACK_CHANNEL,
             'text': "%s for ARN %s" % (detail_type, some_id)
         }
+    elif:
+        event_source = "aws:sns"
+        message = json.loads(event['Records'][0]['Sns']['Message'])
+    
+        alarm_name = message['AlarmName']
+        new_state = message['NewStateValue']
+        reason = message['NewStateReason']
+    
+        slack_message = {
+            'channel': SLACK_CHANNEL,
+            'text': "%s state is now %s: %s" % (alarm_name, new_state, reason)
+        
+    }
     else:
         slack_message = {
             'channel': SLACK_CHANNEL,
